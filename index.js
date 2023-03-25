8a25a04c8f2e9fd8a719caf79b37d93fcd2359b2
Ramia Zulfiqar <razulfiqar1@gmail.com>
2023-03-24 20:37:40 -0400

    lab completed

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+  return tutorials.map(function(line){
+    const words = line.split(" ")
+    const capitalizedWords = words.map(function(word){
+      return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    const response = capitalizedWords.join(" ")
+    return response
+  })
 }
